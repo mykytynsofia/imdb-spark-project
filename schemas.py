e6612715f0c8b3941925b3bb99b091258aaa9506
sofichka <mykytynsofia2003@gmail.com>
2023-12-09 01:03:13 +0100

    Preprocesses title_episode.

--- schemas.py
+++ schemas.py
@@ -2,7 +2,7 @@ import pyspark.sql.types as t
 import columns as c
 import columns.columns_title_principals as columns_title_principals
 import columns.columns_title_basics as columns_title_basics
-
+import columns.columns_title_episode as columns_title_episode
 # --------------------------------------- NAME BASICS -----------------------------------------
 
 # Схема, яка використовуватиметься для першого зчитування датасету
@@ -113,3 +113,24 @@ schema_title_basics_final = t.StructType(
         t.StructField(columns_title_basics.genres, t.StringType(), True),
     ]
 )
+
+# Схема, яка використовуватиметься для першого зчитування датасету
+schema_title_episode = t.StructType(
+    [
+        t.StructField("tconst", t.StringType(), True),
+        t.StructField("parentTconst", t.StringType(), True),
+        t.StructField("seasonNumber", t.IntegerType(), True),
+        t.StructField("episodeNumber", t.IntegerType(), True)
+    ]
+)
+
+# Схема, яка використовуватиметься для вже обробленого датасету
+schema_title_episode_final = t.StructType(
+    [
+        t.StructField(columns_title_episode.tconst, t.StringType(), True),
+        t.StructField(columns_title_episode.parentTconst, t.StringType(), True),
+        t.StructField(columns_title_episode.seasonNumber, t.StringType(), True),
+        t.StructField(columns_title_episode.episodeNumber, t.StringType(), True),
+    ]
+)
+
